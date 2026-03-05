932f7ca2ea1383a9ab38541bec8cfe601a8fa896
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-03-05 15:46:32 +0000

    updated

--- matmul.cpp
+++ matmul.cpp
@@ -1,4 +1,4 @@
-// Fonksiyon 1: Standart üçlü döngü, satır-sütun mantığı
+
 void MatmulNaive(const Matrix<double>& A, const Matrix<double>& B, const Matrix<double>& C){
     auto M = A.row();
     auto N = B.col(); 
@@ -27,4 +27,34 @@ void MatmulNaive_Order(const Matrix<double>& A, const Matrix<double>& B, const M
         }
     }
     // Not: Bu sıralama özellikle row-major düzeni olan matrislerde  memory access açısından daha cache-friendly olabilir
+}
+
+
+
+
+int main(){
+    int M =2, N=4, K=2;
+    Matrix<double> A(M, K);
+    Matrix<double> B(K, N);
+    Matrix<double> C(M, N);
+
+    for(int i=0;i<M;i++){
+        for(int j=0;j<K;j++){
+            A(i,j) = i+j+1; 
+        }
+    }
+    for(int j=0;j<K;j++){
+        for(int k=0;k<N;k++){
+            B(j,k) = j+k+1; 
+        }
+    }
+    MatmulNaive(A, B, C);
+
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            std::cout << C(i,j)<< " ";  
+        }
+        std::cout << std::endl;
+    }
+
 }
